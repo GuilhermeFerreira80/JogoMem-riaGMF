9061baba5f97ab44cc98368c1d4c3b72122b6c4b
GuilhermeFerreira80 <149723734+GuilhermeFerreira80@users.noreply.github.com>
2025-03-04 19:22:01 +0000

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,43 @@
+const inicialContainer=document.getElementById('inicial-container');
+const taskContainer=document.getElementById('task-container');
+let tentativas = [0, 0, 0, 0, 0, 0];
+let maxTentativas = [];
+
+
+function nextAction_1(){
+    inicialContainer.style.display = 'none';
+    
+    taskContainer.style.display = 'grid';
+}
+
+
+
+function inicializarTentativas(qtdCartas) {
+    for (let i = 0; i < qtdCartas; i++) {
+        maxTentativas[i] = Math.floor(Math.random() * 3) + 1; 
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const qtdCartas = document.querySelectorAll(".task-box").length;
+    inicializarTentativas(qtdCartas);
+});
+
+function tentarSubmeter(num) {
+    const containers = document.querySelectorAll(".task-box"); 
+    const buttons = document.querySelectorAll(".task-box .continue-button"); 
+
+    if (num >= containers.length) return; 
+
+    tentativas[num]++; 
+
+    if (tentativas[num] < maxTentativas[num]) {
+        containers[num].style.backgroundColor = "red"; 
+    } else {
+        containers[num].style.backgroundColor = "green"; 
+        buttons[num].style.pointerEvents = "none"; 
+        buttons[num].style.opacity = "0.5"; 
+        tentativas[num] = 0; 
+    }
+}
